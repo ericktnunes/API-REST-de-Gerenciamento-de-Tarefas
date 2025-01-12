b9d5eb0742623768dcf3dbeb996d510285e6d2a6
Erick Tavares <ericktnunes.2004@gmail.com>
2025-01-12 01:16:50 -0300

    add 'import.sql' in resources for testing with h2 database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- Inserir dados na tabela task
+INSERT INTO task (title, description, status, created_at) VALUES ('Tarefa 1', 'Descrição da tarefa 1', 'pending', '2025-01-11');
+INSERT INTO task (title, description, status, created_at) VALUES ('Tarefa 2', 'Descrição da tarefa 2', 'completed', '2025-01-10');
+INSERT INTO task (title, description, status, created_at) VALUES ('Tarefa 3', 'Descrição da tarefa 3', 'pending', '2025-01-09');
+INSERT INTO task (title, description, status, created_at) VALUES ('Tarefa 4', 'Descrição da tarefa 4', 'completed', '2025-01-08');
